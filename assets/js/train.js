4f22bbf3ddebe69d71fb15eb797397483b01f963
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-01-22 09:51:09 -0800

    fixing table hover and resetting template classes.

--- assets/js/train.js
+++ assets/js/train.js
@@ -98,5 +98,5 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   console.log(trainArrival);
 
   // Add each train's data into the table
-  $("#train-table > tbody").append(`<tr><td>${trainName}</td><td> ${trainDestination}</td><td> ${trainFrequency} mins</td><td> ${moment(nextTrain).format("hh:mm")}</td><td> in ${minutesToNextTrain} mins</td><tr>`);
+  $("#train-table > tbody").append(`<tr><td>${trainName}</td><td> ${trainDestination}</td><td> ${trainFrequency} mins</td><td> ${moment(nextTrain).format("hh:mm")}</td><td>in ${minutesToNextTrain} mins</td><tr>`);
 });
