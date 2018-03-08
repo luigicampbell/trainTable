edccff0162f480f78203733d7d607764d51d5e0e
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-03-07 18:38:11 -0800

    Finished fixing authenication, fixed a class to login

--- assets/js/train.js
+++ assets/js/train.js
@@ -5,6 +5,9 @@ let timeApart;
 let nextTrain;
 let minutesToNextTrain;
 
+// Clears object token at load of page
+localStorage.clear();
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyBYVQ3zFv-596GELNTp3dadHQmKlwnbdrM",
@@ -24,7 +27,7 @@ $(".register form").on("submit", function(event){
   // Takes values stored in register, email and password fields
   var email = $(".register .email").val();
   var password = $(".register .password").val();
-
+  console.log('values submitted: ', email, password);
   // Firebase Authentication documentation
   firebase.auth().createUserWithEmailAndPassword(email, password)
   .then(function(user){
@@ -54,93 +57,96 @@ $(".login form").on("submit", function(event){
 
 firebase.auth().onAuthStateChanged(function(user){
   if (user){
-    console.log(`Welcome ${user.name}!`);
-
-    // Ended up deleted for some reason
-    var database = firebase.database();
-
-    // 2. Button for adding Trains
-    $("#add-train-btn").on("click", function(event) {
-
-      // Grabs user input
-      let trainName = $("#train-name-input").val().trim();
-      let trainDestination = $("#destination-input").val().trim();
-      let trainArrival = moment($("#arrival-input").val().trim(), "HH:mm").format("X");
-      let trainFrequency = $("#frequency-input").val().trim();
-      let blah = "blah";
-      // Logging values for form inputs
-      console.log(trainName);
-      console.log(trainDestination);
-      console.log(trainArrival);
-      console.log(trainFrequency);
-
-      // Creates local object for holding train data
-      let newTrain = {
-        trainName: trainName,
-        trainDestination: trainDestination,
-        trainFrequency: trainFrequency,
-        trainArrival: trainArrival,
-      };
-
-      // Uploads train data to the database
-      database.ref().push(newTrain);
-
-      // Logs everything to console
-      console.log(newTrain.trainName);
-      console.log(newTrain.trainDestination);
-      console.log(newTrain.trainFrequency);
-      console.log(newTrain.trainArrival);
-
-      // Clears all of the text-boxes
-      $("#train-name-input").val("");
-      $("#destination-input").val("");
-      $("#frequency-input").val("");
-      $("#arrival-input").val("");
-    });
-
-    // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
-    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-      console.log(childSnapshot.val());
-
-      // Store everything into a variable
-      let trainName = childSnapshot.val().trainName;
-      let trainDestination = childSnapshot.val().trainDestination;
-      let trainArrival = childSnapshot.val().trainArrival;
-      let trainFrequency = childSnapshot.val().trainFrequency;
-
-      // Access Keys
-      trainFrequency = trainFrequency;
-      militaryTime = trainArrival;
-
-      // Convert Military Time
-      militaryTimeConverted = moment(militaryTime, "hh:mm").subtract(1, "year");
-
-      // Current Time
-      currentTime=moment();
-      // Difference in time
-      timeDifference = moment().diff(moment(militaryTimeConverted), "minutes");
-
-      // Time apart
-      timeApart=timeDifference%trainFrequency;
-
-      // minutes until next train
-      minutesToNextTrain=trainFrequency-timeApart;
-
-      // next train
-      nextTrain=moment().add(minutesToNextTrain, "minutes");
-
-      // train Info
-      console.log(`Train Name: ${trainName}`);
-      console.log(`Train Destination: ${trainDestination}`);
-      console.log(`Train Frequency: ${trainFrequency}`);
-      console.log(`Arrival Time: ${trainArrival}`);
-
-      // Add each train's data into the table
-      $("#train-table > tbody").append(`<tr><td>${trainName}</td><td> ${trainDestination}</td><td style="text-align:center">${trainFrequency} mins</td><td style="text-align:center">${moment(nextTrain).format("hh:mm")}</td><td style="text-align:center">in ${minutesToNextTrain} mins</td><tr>`);
-    })
+    console.log(`Welcome ${user.email}!`);
+    // enable add train button
+    $(".add-train-btn").removeAttr("disabled");
   }
   else{
     console.log(`Please logon to proceed`);
   }
 });
+
+
+  // Ended up deleted for some reason
+  var database = firebase.database();
+
+  // 2. Button for adding Trains
+  $(".add-train-btn").on("click", function(event) {
+
+    // Grabs user input
+    let trainName = $("#train-name-input").val().trim();
+    let trainDestination = $("#destination-input").val().trim();
+    let trainArrival = moment($("#arrival-input").val().trim(), "HH:mm").format("X");
+    let trainFrequency = $("#frequency-input").val().trim();
+    let blah = "blah";
+    // Logging values for form inputs
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainArrival);
+    console.log(trainFrequency);
+
+    // Creates local object for holding train data
+    let newTrain = {
+      trainName: trainName,
+      trainDestination: trainDestination,
+      trainFrequency: trainFrequency,
+      trainArrival: trainArrival,
+    };
+
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+
+    // Logs everything to console
+    console.log(newTrain.trainName);
+    console.log(newTrain.trainDestination);
+    console.log(newTrain.trainFrequency);
+    console.log(newTrain.trainArrival);
+
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#frequency-input").val("");
+    $("#arrival-input").val("");
+  });
+
+  // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+    console.log(childSnapshot.val());
+
+    // Store everything into a variable
+    let trainName = childSnapshot.val().trainName;
+    let trainDestination = childSnapshot.val().trainDestination;
+    let trainArrival = childSnapshot.val().trainArrival;
+    let trainFrequency = childSnapshot.val().trainFrequency;
+
+    // Access Keys
+    trainFrequency = trainFrequency;
+    militaryTime = trainArrival;
+
+    // Convert Military Time
+    militaryTimeConverted = moment(militaryTime, "hh:mm").subtract(1, "year");
+
+    // Current Time
+    currentTime=moment();
+    // Difference in time
+    timeDifference = moment().diff(moment(militaryTimeConverted), "minutes");
+
+    // Time apart
+    timeApart=timeDifference%trainFrequency;
+
+    // minutes until next train
+    minutesToNextTrain=trainFrequency-timeApart;
+
+    // next train
+    nextTrain=moment().add(minutesToNextTrain, "minutes");
+
+    // train Info
+    console.log(`Train Name: ${trainName}`);
+    console.log(`Train Destination: ${trainDestination}`);
+    console.log(`Train Frequency: ${trainFrequency}`);
+    console.log(`Arrival Time: ${trainArrival}`);
+
+    // Add each train's data into the table
+    $("#train-table > tbody").append(`<tr><td>${trainName}</td><td> ${trainDestination}</td><td style="text-align:center">${trainFrequency} mins</td><td style="text-align:center">${moment(nextTrain).format("hh:mm")}</td><td style="text-align:center">in ${minutesToNextTrain} mins</td><tr>`);
+  });
